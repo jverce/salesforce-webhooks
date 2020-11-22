237910dc95c2467067722570f852df6e6667a0f1
Jay Vercellone <juanjov@gmail.com>
2020-11-22 13:07:23 -0800

    Initial package structure

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai');
+const { describe } = require('mocha');
+
+describe('Main entry point', function() {
+
+  it('should do nothing', async function() {
+    assert.isTrue(true);
+  });
+
+});
