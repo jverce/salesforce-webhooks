0d3d035163554b09f4fad96e53e2af83e0c0afe8
Jay Vercellone <juanjov@gmail.com>
2021-06-16 20:06:21 -0700

    Fix test coverage reports

--- test/unit/client.test.js
+++ test/unit/client.test.js
@@ -4,7 +4,7 @@ import chai,
 import chaiAsPromised from "chai-as-promised";
 import { describe } from "mocha";
 import sinon from "sinon";
-import { SalesforceClient } from "../../src/client";
+import { SalesforceClient } from "../../src";
 import { remoteSiteSettings } from "./data/soap-responses";
 
 chai.use(chaiAsPromised);
